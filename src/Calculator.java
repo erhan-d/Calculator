35a356cf3cf1896e59fef611f75637503d06e20e
erhan-d <dolapcierhan@hotmail.com>
2019-11-23 15:48:40 -0500

    conflic resolved

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,7 @@
 public class Calculator {
-<<<<<<< HEAD
+
     // this is.....
-=======
-    // commit...
->>>>>>> 0bce09e10cf6c83cc9208e2b5225eabad512fb23
-    public static double add(double num1, double num2){
+  public static double add(double num1, double num2){
         return num1 + num2;
     }
     public static double substract(double num1, double num2){
